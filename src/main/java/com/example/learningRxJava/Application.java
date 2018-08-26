7162da3c457ad3aa57f3ec366c09e5839c35a355
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-08-26 10:58:30 +0900

    Observable.just()を使って単純化。

--- src/main/java/com/example/learningRxJava/Application.java
+++ src/main/java/com/example/learningRxJava/Application.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import rx.Observable;
-import rx.Observable.OnSubscribe;
-import rx.Observer;
-import rx.Subscriber;
 
 @SpringBootApplication
 public class Application {
@@ -21,67 +18,27 @@ public class Application {
         }
     }
 
-    private Observable<String> createObservable() {
-        return Observable.create(
-                new OnSubscribe<String>() {
-
-                    @Override
-                    public void call(Subscriber<? super String> subscriber) {
-                        // 購読解除されている場合は処理をやめる
-                        if (subscriber.isUnsubscribed()) {
-                            return;
-                        }
-
-                        // 1回目の通知をする
-                        subscriber.onNext("Hello, World!");
-                        // 2回目の通知をする
-                        subscriber.onNext("こんにちは、世界！");
-
-                        // 購読解除されていない場合
-                        if (!subscriber.isUnsubscribed()) {
-                            // 完了したことを通知する
-                            subscriber.onCompleted();
-                        }
-                    }
-                });
-    }
-
-    private Observer<String> createObserver() {
-        return new Observer<String>() {
-
-            // Observableからのデータを受け取った際の処理
-            @Override
-            public void onNext(String item) {
-                // 実行しているThread名の取得
-                String threadName = Thread.currentThread().getName();
-                // Observableからのデータをそのまま標準出力する
-                System.out.println(threadName + ": " + item);
-            }
-
-            // Observableから完了を通知された際の処理
-            @Override
-            public void onCompleted() {
-                // 実行しているThread名の取得
-                String threadName = Thread.currentThread().getName();
-                System.out.println(threadName + ": 完了しました");
-            }
-
-            // Observableからエラーを通知された際の処理
-            @Override
-            public void onError(Throwable e) {
-                e.printStackTrace();
-            }
-        };
-    }
-
     private void run() throws Exception {
         // あいさつの言葉を通知するObservableの生成
-        Observable<String> observableGreeting = createObservable();
-
-        // Observerの生成
-        Observer<String> observer = createObserver();
-
-        // Observableを購読し処理を開始する
-        observableGreeting.subscribe(observer);
+        Observable.just("Hello, World!", "こんにちは、世界！")
+                // Observableを購読し処理を開始する
+                .subscribe(
+                        // Observableからのデータを受け取った際の処理
+                        item -> {
+                            // 実行しているThread名の取得
+                            String threadName = Thread.currentThread().getName();
+                            // Observableからのデータをそのまま標準出力する
+                            System.out.println(threadName + ": " + item);
+                        },
+                        // Observableからエラーを通知された際の処理
+                        error -> {
+                            error.printStackTrace();
+                        },
+                        // Observableから完了を通知された際の処理
+                        () -> {
+                            // 実行しているThread名の取得
+                            String threadName = Thread.currentThread().getName();
+                            System.out.println(threadName + ": 完了しました");
+                        });
     }
 }
