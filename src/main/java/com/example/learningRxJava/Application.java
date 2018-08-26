b70a260ef3395172fcafd5381d4e72fc237a253d
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-08-26 11:11:05 +0900

    非同期処理を実装。

--- src/main/java/com/example/learningRxJava/Application.java
+++ src/main/java/com/example/learningRxJava/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import rx.Observable;
+import rx.Observer;
+import rx.schedulers.Schedulers;
 
 @SpringBootApplication
 public class Application {
@@ -21,24 +23,44 @@ public class Application {
     private void run() throws Exception {
         // あいさつの言葉を通知するObservableの生成
         Observable.just("Hello, World!", "こんにちは、世界！")
+                // 通知後の処理を非同期で行うためのSchedulerを設定
+                .observeOn(Schedulers.computation()) // ・・・（1）
                 // Observableを購読し処理を開始する
                 .subscribe(
-                        // Observableからのデータを受け取った際の処理
-                        item -> {
-                            // 実行しているThread名の取得
-                            String threadName = Thread.currentThread().getName();
-                            // Observableからのデータをそのまま標準出力する
-                            System.out.println(threadName + ": " + item);
-                        },
-                        // Observableからエラーを通知された際の処理
-                        error -> {
-                            error.printStackTrace();
-                        },
-                        // Observableから完了を通知された際の処理
-                        () -> {
-                            // 実行しているThread名の取得
-                            String threadName = Thread.currentThread().getName();
-                            System.out.println(threadName + ": 完了しました");
+                        new Observer<String>() {
+
+                            // Observableからのデータを受け取った際の処理
+                            @Override
+                            public void onNext(String item) {
+                                // 実行しているThread名の取得
+                                String threadName = Thread.currentThread().getName();
+                                // Observableからのデータをそのまま標準出力する
+                                System.out.println(threadName + ": " + item);
+                            }
+
+                            // Observableから完了を通知された際の処理
+                            @Override
+                            public void onCompleted() {
+                                // 実行しているThread名の取得
+                                String threadName = Thread.currentThread().getName();
+                                System.out.println(threadName + ": 完了しました");
+                            }
+
+                            // Observableからエラーを通知された際の処理
+                            @Override
+                            public void onError(Throwable e) {
+                                e.printStackTrace();
+                            }
                         });
+
+        // 非同期で行われていることを確認するため出力する
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + ": subscribed!");
+
+        // しばらく待つ
+        Thread.sleep(5000L); // ・・・（2）
+
+        // 終了メッセージ
+        System.out.println(threadName + ": end!");        
     }
 }
